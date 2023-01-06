f38f6db99329aff9e2fd743fd1d5f8e92be20e30
Jonny Tesfahun AK <104738883+Jonny-T87@users.noreply.github.com>
2023-01-05 21:42:33 -0800

    update flask

--- flask/main.py
+++ flask/main.py
@@ -1,12 +1,12 @@
-from flask import Flask, escape, request, render_template
+from flask import Flask, render_template
 from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    utc_time = datetime.utcnow().strftime('Y%-%m-%d %H:%M:%S')
+    utc_time = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
     return render_template("index.html", utc_time=utc_time)
 
-if __name__ == '__main__':
+if __name__=='__main__':
     app.run(debug=True)
